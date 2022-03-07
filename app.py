860a8258111b57458010bd82c2fb0c6b7922c242
Graham Morby <graham.morby@leighton.com>
2022-03-07 15:42:26 +0000

    all templates now use a template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/project')
+def project():
+    return render_template('detail.html')
+
+
+@app.route('/add/project')
+def add_project():
+    return render_template('projectform.html')
 
 
 if __name__ == '__main__':
