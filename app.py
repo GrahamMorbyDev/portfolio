9e3453f79785294b0b38c191e73519e18d8582d4
Graham Morby <graham.morby@leighton.com>
2022-03-08 11:56:12 +0000

    Projects are on index and single project view pages

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import render_template, url_for
+from flask import (render_template, url_for, request, redirect)
 from models import db, app, Project
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    projects = Project.query.all()
+    return render_template('index.html', projects=projects)
 
 
 @app.route('/about')
@@ -12,18 +13,28 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/project')
-def project():
-    return render_template('detail.html')
+@app.route('/project/<id>')
+def project(id):
+    single_project = Project.query.get(id)
+    return render_template('detail.html', project=single_project)
 
 
-@app.route('/add/project')
+@app.route('/add/project', methods=['GET', 'POST'])
 def add_project():
+    if request.form:
+        new_project = Project(
+            title=request.form['title'],
+            description=request.form['desc'],
+            link=request.form['github'],
+            skills=request.form['skills'],
+            completed=request.form['date']
+        )
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('projectform.html')
 
 
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
-
-
